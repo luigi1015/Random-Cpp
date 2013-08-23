649324f7300c49a66f822573742133122d1fe384
luigi1015 <github@314ghz.com>
2013-08-23 13:13:32 -0500

    Initial working version with random unsigned longs, random hex digits, random alphanumeric characters and random non-whie space printable characters.

--- random.cpp
+++ random.cpp
@@ -4,7 +4,7 @@
 
 namespace RNG
 {//RNG for Random Number Generator
-	std::vector<unsigned long> generateNumber( unsigned long min, unsigned long max )
+	unsigned long generateNumber( unsigned long min, unsigned long max )
 	{//Generate and returns a single random unsigned long between min and max.
 		std::uniform_int_distribution<unsigned long> uid(min, max);
 		std::random_device rd;
@@ -16,24 +16,17 @@ namespace RNG
 	std::vector<unsigned long> generateNums( unsigned long min, unsigned long max, unsigned long length )
 	{//Generate length number of random unsigned longs between min and max. Returns a vector of the random unsigned longs.
 		std::vector<unsigned long> nums;
-/*
-		std::uniform_int_distribution<unsigned long> uid(min, max);
-		std::random_device rd;
-		std::mt19937 generator(rd());
-		auto rng = std::bind( uid, generator );//Create a function to create random unsigned integers.
-*/
 		
 		for( unsigned long i = 0; i < length; i++ )
 		{//Add length number of random integers by calling generateNumber length times.
-			//nums.push_back( rng() );
 			nums.push_back(  generateNumber(min, max) );
 		}
 		
 		return nums;
 	}
 
-	std::vector<char> generateHexString( unsigned long length )
-	{//Generate length number of random chararcters from the groups 0...9 and A...F according to their hex values. Returns a vector of the random unsigned longs.
+	std::vector<char> generateHexVector( unsigned long length )
+	{//Generate length number of random chararcters from the groups 0...9 and A...F according to their hex values. Returns a vector of the characters.
 		std::vector<char> hexValues;//Vector to hold the possible hex values.
 		std::vector<char> hexChars;//Vector of the random hex characters.
 
@@ -57,22 +50,195 @@ namespace RNG
 
 		for( unsigned long i = 0; i < length; i++ )
 		{//Add length number of random hex characters by calling generateNumber length times and passing it through hexValues.
-			//nums.push_back( rng() );
 			hexChars.push_back(  hexValues.at(generateNumber(0, hexValues.size()-1)) );
 		}
-/*
-		std::vector<unsigned long> nums;
-		std::uniform_int_distribution<unsigned long> uid(min, max);
-		std::random_device rd;
-		std::mt19937 generator(rd());
-		auto rng = std::bind( uid, generator );//Create a function to create random unsigned integers.
 		
+		return hexChars;
+	}
+
+	std::vector<char> generateAlphaNumericVector( unsigned long length )
+	{//Generate length number of random chararcters from the groups 0...9 and A...Z and a...z. Returns a vector of the characters.
+		std::vector<char> alphaNumericValues;//Vector to hold the possible hex values.
+		std::vector<char> alphaNumericChars;//Vector of the random hex characters.
+
+		//Populate the possible hex values.
+		alphaNumericValues.push_back('0');
+		alphaNumericValues.push_back('1');
+		alphaNumericValues.push_back('2');
+		alphaNumericValues.push_back('3');
+		alphaNumericValues.push_back('4');
+		alphaNumericValues.push_back('5');
+		alphaNumericValues.push_back('6');
+		alphaNumericValues.push_back('7');
+		alphaNumericValues.push_back('8');
+		alphaNumericValues.push_back('9');
+		alphaNumericValues.push_back('A');
+		alphaNumericValues.push_back('B');
+		alphaNumericValues.push_back('C');
+		alphaNumericValues.push_back('D');
+		alphaNumericValues.push_back('E');
+		alphaNumericValues.push_back('F');
+		alphaNumericValues.push_back('G');
+		alphaNumericValues.push_back('H');
+		alphaNumericValues.push_back('I');
+		alphaNumericValues.push_back('J');
+		alphaNumericValues.push_back('K');
+		alphaNumericValues.push_back('L');
+		alphaNumericValues.push_back('M');
+		alphaNumericValues.push_back('N');
+		alphaNumericValues.push_back('O');
+		alphaNumericValues.push_back('P');
+		alphaNumericValues.push_back('Q');
+		alphaNumericValues.push_back('R');
+		alphaNumericValues.push_back('S');
+		alphaNumericValues.push_back('T');
+		alphaNumericValues.push_back('U');
+		alphaNumericValues.push_back('V');
+		alphaNumericValues.push_back('W');
+		alphaNumericValues.push_back('X');
+		alphaNumericValues.push_back('Y');
+		alphaNumericValues.push_back('Z');
+		alphaNumericValues.push_back('a');
+		alphaNumericValues.push_back('b');
+		alphaNumericValues.push_back('c');
+		alphaNumericValues.push_back('d');
+		alphaNumericValues.push_back('e');
+		alphaNumericValues.push_back('f');
+		alphaNumericValues.push_back('g');
+		alphaNumericValues.push_back('h');
+		alphaNumericValues.push_back('i');
+		alphaNumericValues.push_back('j');
+		alphaNumericValues.push_back('k');
+		alphaNumericValues.push_back('l');
+		alphaNumericValues.push_back('m');
+		alphaNumericValues.push_back('n');
+		alphaNumericValues.push_back('o');
+		alphaNumericValues.push_back('p');
+		alphaNumericValues.push_back('q');
+		alphaNumericValues.push_back('r');
+		alphaNumericValues.push_back('s');
+		alphaNumericValues.push_back('t');
+		alphaNumericValues.push_back('u');
+		alphaNumericValues.push_back('v');
+		alphaNumericValues.push_back('w');
+		alphaNumericValues.push_back('x');
+		alphaNumericValues.push_back('y');
+		alphaNumericValues.push_back('z');
+
 		for( unsigned long i = 0; i < length; i++ )
-		{//Add length number of hex characters.
-			nums.push_back( rng() );
+		{//Add length number of random hex characters by calling generateNumber length times and passing it through hexValues.
+			alphaNumericChars.push_back(  alphaNumericValues.at(generateNumber(0, alphaNumericValues.size()-1)) );
 		}
 		
-		return nums;
-*/
+		return alphaNumericChars;
+	}
+
+	std::vector<char> generateNonWhiteSpacePrintableVector( unsigned long length )
+	{//Generate length number of random non-white space printable ascii chararcters. Returns a vector of the characters.
+		std::vector<char> printableValues;//Vector to hold the possible printable values.
+		std::vector<char> printableChars;//Vector of the random printable characters.
+
+		//Populate the possible printable values.
+		printableValues.push_back('!');
+		printableValues.push_back('"');
+		printableValues.push_back('#');
+		printableValues.push_back('$');
+		printableValues.push_back('%');
+		printableValues.push_back('&');
+		printableValues.push_back('\'');
+		printableValues.push_back('(');
+		printableValues.push_back(')');
+		printableValues.push_back('*');
+		printableValues.push_back('+');
+		printableValues.push_back(',');
+		printableValues.push_back('-');
+		printableValues.push_back('.');
+		printableValues.push_back('/');
+		printableValues.push_back('0');
+		printableValues.push_back('1');
+		printableValues.push_back('2');
+		printableValues.push_back('3');
+		printableValues.push_back('4');
+		printableValues.push_back('5');
+		printableValues.push_back('6');
+		printableValues.push_back('7');
+		printableValues.push_back('8');
+		printableValues.push_back('9');
+		printableValues.push_back(':');
+		printableValues.push_back(';');
+		printableValues.push_back('<');
+		printableValues.push_back('=');
+		printableValues.push_back('>');
+		printableValues.push_back('?');
+		printableValues.push_back('@');
+		printableValues.push_back('A');
+		printableValues.push_back('B');
+		printableValues.push_back('C');
+		printableValues.push_back('D');
+		printableValues.push_back('E');
+		printableValues.push_back('F');
+		printableValues.push_back('G');
+		printableValues.push_back('H');
+		printableValues.push_back('I');
+		printableValues.push_back('J');
+		printableValues.push_back('K');
+		printableValues.push_back('L');
+		printableValues.push_back('M');
+		printableValues.push_back('N');
+		printableValues.push_back('O');
+		printableValues.push_back('P');
+		printableValues.push_back('Q');
+		printableValues.push_back('R');
+		printableValues.push_back('S');
+		printableValues.push_back('T');
+		printableValues.push_back('U');
+		printableValues.push_back('V');
+		printableValues.push_back('W');
+		printableValues.push_back('X');
+		printableValues.push_back('Y');
+		printableValues.push_back('Z');
+		printableValues.push_back('[');
+		printableValues.push_back('\\');
+		printableValues.push_back(']');
+		printableValues.push_back('^');
+		printableValues.push_back('_');
+		printableValues.push_back('`');
+		printableValues.push_back('a');
+		printableValues.push_back('b');
+		printableValues.push_back('c');
+		printableValues.push_back('d');
+		printableValues.push_back('e');
+		printableValues.push_back('f');
+		printableValues.push_back('g');
+		printableValues.push_back('h');
+		printableValues.push_back('i');
+		printableValues.push_back('j');
+		printableValues.push_back('k');
+		printableValues.push_back('l');
+		printableValues.push_back('m');
+		printableValues.push_back('n');
+		printableValues.push_back('o');
+		printableValues.push_back('p');
+		printableValues.push_back('q');
+		printableValues.push_back('r');
+		printableValues.push_back('s');
+		printableValues.push_back('t');
+		printableValues.push_back('u');
+		printableValues.push_back('v');
+		printableValues.push_back('w');
+		printableValues.push_back('x');
+		printableValues.push_back('y');
+		printableValues.push_back('z');
+		printableValues.push_back('{');
+		printableValues.push_back('|');
+		printableValues.push_back('}');
+		printableValues.push_back('~');
+
+		for( unsigned long i = 0; i < length; i++ )
+		{//Add length number of random hex characters by calling generateNumber length times and passing it through hexValues.
+			printableChars.push_back(  printableValues.at(generateNumber(0, printableValues.size()-1)) );
+		}
+		
+		return printableChars;
 	}
 }
--- test.cpp
+++ test.cpp
@@ -3,19 +3,33 @@
 #include <iostream>
 #include <string> 
 
-template <class T> void printValues( std::vector<T> items )
+template <class T> void printValuesWithSpace( std::vector<T> items )
 {//Print off the items in items.
 	if( items.size() == 0 )
 	{
-		std::cerr << "In printValues, trying to print a vector with nothing in it." << endl;
-		break;
+		std::cerr << "In printValues, trying to print a vector with nothing in it." << std::endl;
+		return;
 	}
 
-	std::cout << randNums.at(0);//Print off the first character without a space.
+	std::cout << items.at(0);//Print off the first character without a space.
 
-	for( unsigned long i = 1; i < randNums.size(); i++ )
+	for( unsigned long i = 1; i < items.size(); i++ )
 	{//Print off the last characters.
-		std::cout << " " << randNums.at(i);
+		std::cout << " " << items.at(i);
+	}
+}
+
+template <class T> void printValuesNoSpace( std::vector<T> items )
+{//Print off the items in items.
+	if( items.size() == 0 )
+	{
+		std::cerr << "In printValues, trying to print a vector with nothing in it." << std::endl;
+		return;
+	}
+
+	for( unsigned long i = 0; i < items.size(); i++ )
+	{//Print off the last characters.
+		std::cout << items.at(i);
 	}
 }
 
@@ -35,14 +49,28 @@ int main( int argc, const char* argv[] )
 		std::vector<unsigned long> randNums = RNG::generateNums( min, max, length );
 
 		//Generate the random hex characters.
-		std::vector<char> randHex = RNG::generateHexString( length );
+		std::vector<char> randHex = RNG::generateHexVector( length );
+
+		//Generate the random alphanumeric characters.
+		std::vector<char> randAlphaNumeric = RNG::generateAlphaNumericVector( length );
+
+		//Generate the random alphanumeric characters.
+		std::vector<char> randPrintable = RNG::generateNonWhiteSpacePrintableVector( length );
 	
-		std::cout << "Random Numbers:" << std::endl;
-		printValues( randNums );
-		std::cout << std::endl;
+		std::cout << "Random Numbers in between " << min << " and " << max << " inclusive." << std::endl;
+		printValuesWithSpace( randNums );
+		std::cout << std::endl << std::endl;
 	
 		std::cout << "Random Hex Number:" << std::endl;
-		printValues( randHex );
+		printValuesNoSpace( randHex );
+		std::cout << std::endl << std::endl;
+	
+		std::cout << "Random AlphaNumeric Characters:" << std::endl;
+		printValuesNoSpace( randAlphaNumeric );
+		std::cout << std::endl << std::endl;
+	
+		std::cout << "Random Printable Characters:" << std::endl;
+		printValuesNoSpace( randPrintable );
 		std::cout << std::endl;
 	}
 	
